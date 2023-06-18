3bedebe34e2313641f4e967b6443133577737604
chaleamsuk-developer <chaleamsuk.sremaung@gmail.com>
2023-06-18 11:28:47 +0700

    init project

---
+++ hero_api/heros/models.py
@@ -0,0 +1,9 @@
+# heros/models.py
+from django.db import models
+
+class Hero(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
---
+++ hero_api/heros/views.py
@@ -0,0 +1,12 @@
+# heros/views.py
+from rest_framework import generics
+from .models import Hero
+from .serializers import HeroSerializer
+
+class ListHero(generics.ListCreateAPIView):
+    queryset = Hero.objects.all()
+    serializer_class = HeroSerializer
+    
+class DetailHero(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Hero.objects.all()
+    serializer_class = HeroSerializer 
